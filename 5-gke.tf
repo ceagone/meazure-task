30980703a43c2ce93e147e66bd8f3be6453f5704
aogunfowora <90580408+aogunfowora@users.noreply.github.com>
2021-09-12 23:19:23 -0500

    Update 5-gke.tf

--- 5-gke.tf
+++ 5-gke.tf
@@ -72,8 +72,7 @@ resource "null_resource" "openfaas-task" {
     kubectl create ns openfaas-fn
     kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
     helm repo add openfaas https://openfaas.github.io/faas-netes/
-    helm repo update \
-      && helm upgrade openfaas --install openfaas/openfaas --namespace openfaas --set functionNamespace=openfaas-fn --set generateBasicAuth=true --set serviceType=LoadBalancer
+    helm repo update && helm upgrade openfaas --install openfaas/openfaas --namespace openfaas --set functionNamespace=openfaas-fn --set generateBasicAuth=true --set serviceType=LoadBalancer
     PASSWORD=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode) && echo "OpenFaaS admin password: $PASSWORD" >> password.txt
     EOF
   }
